12a89cd5259bb1429ccf8feca82aef53c4c4e1c8
RV Mendoza <RVxMendoza@gmail.com>
2015-04-10 15:43:13 -0400

    Add metalsmith watch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Metalsmith = require('metalsmith');
 var markdown   = require('metalsmith-markdown');
 var templates   = require('metalsmith-templates');
+var watch   = require('metalsmith-watch');
 
 
 Metalsmith(__dirname)
@@ -8,6 +9,10 @@ Metalsmith(__dirname)
 	.use(markdown())
 	.use(templates('handlebars'))
 	.destination('./build')
+	.use(watch({
+		pattern : '**/*',
+		livereload: true
+	}))
 	.build(function(err, files) {
 				console.log('built')
 				if (err) { throw err; }
