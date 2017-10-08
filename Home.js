4580102f288a25d604387f01ce658cd6db418127
Tito Mendoza <titomendoza@Tito-Mendoza.local>
2017-10-07 22:54:00 -0400

    Try out shed.css

--- Home.js
+++ Home.js
@@ -5,46 +5,51 @@ import { createContainer, query } from "@phenomic/preset-react-app/lib/client";
 
 import Layout from "./Layout";
 
+import "./main.css";
+
 const Home = ({ isLoading, posts }) => (
     <Layout>
-      <Head>
+        <Head>
         <title>Hello world!</title>
         <meta name="description" content="Everything is awesome!" />
-      </Head>
-      <h1>Home OK SO YES </h1>
-      {isLoading && "Loading..."}
-      {!isLoading && (
+        </Head>
+        <header className="t-a:c">
+            <h1>RV Mendoza</h1>
+        </header>
+
+        {isLoading && "Loading..."}
+        {!isLoading && (
         <ul>
-          {posts &&
+            {posts &&
             posts.node &&
             posts.node.list &&
             posts.node.list.map(post => (
-              <li key={post.id}>
+                <li key={post.id}>
                 <Link to={`/blog/${post.id}/`}>{post.title || post.id}</Link>
-              </li>
+                </li>
             ))}
         </ul>
-      )}
-      <div>
-        {posts.node &&
-        posts.node.hasPreviousPage && (
-          <Link
-            to={
-              posts.node.previousPageIsFirst ? (
-                `/`
-              ) : (
-                `/after/${posts.node.previous}/`
-              )
-            }
-          >
-            Newer posts
-          </Link>
-        )}{" "}
-        {posts.node &&
-        posts.node.hasNextPage && (
-          <Link to={`/after/${posts.node.next}/`}>Older posts</Link>
         )}
-      </div>
+        <div>
+        {posts.node &&
+            posts.node.hasPreviousPage && (
+                <Link
+                to={
+                    posts.node.previousPageIsFirst ? (
+                    `/`
+                    ) : (
+                    `/after/${posts.node.previous}/`
+                    )
+                }
+                >
+                Newer posts
+                </Link>
+            )}{" "}
+            {posts.node &&
+            posts.node.hasNextPage && (
+                <Link to={`/after/${posts.node.next}/`}>Older posts</Link>
+            )}
+        </div>
     </Layout>
   );
   
