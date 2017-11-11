97a11aa5551b71d263af87b8753271294df91085
RV Mendoza <RVxMendoza@gmail.com>
2017-11-11 13:03:14 -0500

    Add copy changes per mentor's suggestions

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -9,19 +9,22 @@ class Bio extends React.Component {
 			<div className="Bio u-paddingRm">
 				<h2 className="Bio-highlight u-marginBs">Finding someone that can code is easy.</h2>
 				<p className="u-marginBs">
-					But what about finding a <span className="u-textBold">front-end/UI developer</span> that can merge into your project management and work with your designers and back-end developers to deliver a user-interface that's responsive, modular, and future friendly?
+					But what about finding a <span className="u-textBold">front-end developer</span> that can merge into your project management and work with your designers and back-end developers to deliver a user-interface that works for everyone, everywhere, every time?
 				</p>
 				<p className="Bio-highlight Bio-highlight--impact u-marginBb">
 					That's where I come in.
 				</p>
 				<p className="u-marginBs">
-					I work with clients who want to stop wasting their time and money on developers that deliver terrible, unmaintainable front-end code.
+					I work with clients who want to stop wasting their time and money on front-end web developers that deliver <span className="u-textBold">unmaintainable code that causes them more work</span> in the future.
 				</p>
 				<p className="u-marginBs">
-					My user-first approach has brought teams to win <a href="http://myql.com/">JD Power Awards for our responsive web apps</a>, and Google’s “Game Changer” award for creating a <a href="https://leveleleven.com/">Fitbit for sales</a>.
+					My user-first approach to user interface development has empowered organizations from <a href="https://www.quickenloans.com/">multi-billion dollar financial institutions</a>, to <a href="https://leveleleven.com/">start-ups looking to pioneer an uncharted industry</a>.
+				</p>
+				<p>
+					I’ve helped teams win an array of accolades like <a href="https://www.myql.com/">JD Power Awards for our responsive web apps</a>, and Google’s “Game Changer” award for creating a <a href="https://leveleleven.com/product-tour/">Fitbit for sales.</a>
 				</p>
 				<p className="u-marginBs">
-					I specialize in HTML, CSS, and ReactJS to help you build modular and semantic web components for the modern browser.
+					I specialize in <a href="https://github.com/RVMendoza">HTML, CSS, and ReactJS</a> to help you build modular and semantic web components for the modern browser.
 				</p>
 				<p className="u-marginBs">
 					I’m Agile ready and available for freelance remote work—if you would like to build something together, feel free to <a className="u-textBold" href="mailto:RVxMendoza@gmail.com">get in touch</a>.
