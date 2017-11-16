edacbf6751f4c47e1a0138b14cbccb00cede2675
RV Mendoza <RVxMendoza@gmail.com>
2017-11-16 15:30:37 -0500

    Add thaw page and make it work!

--- src/pages/thaw.js
+++ src/pages/thaw.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
+import Thaw2018 from './thaw-2018.jpg'
 
 class Thaw extends React.Component {
 	render() {
@@ -11,59 +12,42 @@ class Thaw extends React.Component {
 		return (
 			<div>
 				<Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
-				<div className="App-body u-marginBg">
-          <h2 className="Headline Highlight u-textUppercase u-textBold u-marginBm">Thaw 2018</h2>
-					<ul className="u-notFlush u-marginBm">
-						<li>
-							<span className="u-textBold u-textUppercase">When: </span>
-							Friday, January 19th, 2018 - 9PM
-						</li>
-						<li>
-							<span className="u-textBold u-textUppercase">Where: </span>
-							El Club - 4114 Vernor Hwy, Detroit, MI 48209
-						</li>
-						<li>
-							<span className="u-textBold u-textUppercase">Tickets: </span>
-							Available soon
-						</li>
-					</ul>
+				<div className="App-body u-marginBb">
+					<main className="u-paddingRm">
+						<h2 className="Headline Highlight u-textUppercase u-textBold u-marginBm">Thaw 2018</h2>
+						<ul className="u-notFlush u-marginBm">
+							<li>
+								<span className="u-textBold u-textUppercase">When: </span>
+								Friday, January 19th, 2018 - 9PM
+							</li>
+							<li>
+								<span className="u-textBold u-textUppercase">Where: </span>
+								El Club - 4114 Vernor Hwy, Detroit, MI 48209
+							</li>
+							<li>
+								<span className="u-textBold u-textUppercase">Tickets: </span>
+								Available soon
+							</li>
+						</ul>
 
-					<p className="u-notFlush u-marginBm">
-						In the winter time, people have a tendency to isolate themselves and become hardened.
-					</p>
+						<p className="u-notFlush u-marginBm">
+							In the winter time, people have a tendency to isolate themselves and become hardened.
+						</p>
 
-					<p className="u-notFlush u-marginBm">
-						THAW is a performance and dance party. It's a wonderful night full of audio/visual performances that creates a Queer and POC centered space where people can thrive in the relentlessly bitter and unforgiving winter months.
-					</p>
+						<p className="u-notFlush u-marginBm">
+							THAW is a performance and dance party. It's a wonderful night full of audio/visual performances that creates a Queer and POC centered space where people can thrive in the relentlessly bitter and unforgiving winter months.
+						</p>
 
-					<p className="u-notFlush u-marginBm">
-						All are welcome to enjoy! All that's asked is that we respect each other.
-					</p>
+						<p className="u-notFlush u-marginBm">
+							All are welcome to enjoy! All that's asked is that we respect each other.
+						</p>
 
-					<p className="u-notFlush u-marginBg">
-						Follow <a href="https://www.instagram.com/rvxmendoza/">RV Mendoza on Instagram</a> for updates and checkout <a href="https://www.instagram.com/explore/tags/thawdetroit/">#ThawDetroit</a> to see what you're in for.
-					</p>
-
-					<img className="u-sizeFull" src="thaw-2018.jpg" />
+						<p className="u-notFlush u-marginBm">
+							Follow <a href="https://www.instagram.com/rvxmendoza/">RV Mendoza on Instagram</a> for updates and checkout <a href="https://www.instagram.com/explore/tags/thawdetroit/">#ThawDetroit</a> to see what you're in for.
+						</p>
+					</main>
+					<img className="u-sizeFull" src={Thaw2018}/>
 				</div>
-				{/*
-        {posts.map(post => {
-          if (post.node.path !== '/404/') {
-            const title = get(post, 'node.frontmatter.title') || post.node.path
-            return (
-              <div key={post.node.frontmatter.path}>
-                <h3>
-                  <Link to={post.node.frontmatter.path} >
-                    {post.node.frontmatter.title}
-                  </Link>
-                </h3>
-                <small>{post.node.frontmatter.date}</small>
-                <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-              </div>
-            )
-          }
-				})}
-			*/}
 			</div>
 		)
 	}
